0282014935cf45ce8212758233f59cf9224aa9cc
MIBAE-SEO <66666099+MIBAE-SEO@users.noreply.github.com>
2020-07-15 13:12:48 +0900

    Add files via upload

--- OrderMgrServiceImpl.java
+++ OrderMgrServiceImpl.java
@@ -3,20 +3,24 @@ package kr.co.sol.admin.service.impl;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import kr.co.sol.admin.dao.OrderMgrDAO;
 import kr.co.sol.admin.service.OrderMgrService;
 import kr.co.sol.shop.dto.OrderDTO;
 
-public class OrderMgrServiceImpl implements OrderMgrService{
 	
+@Service
+public class OrderMgrServiceImpl implements OrderMgrService {
+		
 	@Autowired  
 	OrderMgrDAO orderMgrDao;
 
 
 	@Override
 	public List<OrderDTO> getOrders(OrderDTO odto) {
-		return orderMgrDao.getOrders(odto);
-	}
+	return orderMgrDao.getOrders(odto);
+		}
 
 }
+
